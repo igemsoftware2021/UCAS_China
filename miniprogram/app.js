5f5684121551e640371c3fb8f297c609851de2dc
ETOgaosion <gaoziyuan19@mails.ucas.ac.cn>
2021-04-19 19:55:47 +0800

    change record page to recommand

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,5 +1,11 @@
 //app.js
 App({
+  globalData:{
+    userInfo: {},
+    hasUserInfo: false,
+    canIUseGetUserProfile: false,
+  },
+
   onLaunch: function () {
     if (!wx.cloud) {
       console.error('请使用 2.2.3 或以上的基础库以使用云能力')
@@ -13,7 +19,19 @@ App({
         traceUser: true,
       })
     }
-
-    this.globalData = {}
-  }
+    if (wx.getUserProfile) {
+      this.canIUseGetUserProfile =true
+    }
+  },
+  getUserProfile(e) {
+    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认
+    // 开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
+    wx.getUserProfile({
+      desc: '用于为您提供个性化方案', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      success: (res) => {
+        this.userInfo= res.userInfo,
+        this.hasUserInfo= true
+      }
+    })
+  },
 })
